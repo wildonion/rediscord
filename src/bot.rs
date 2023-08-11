83e8c0752c7957f043b9aa114f9e58f02ed1c660
dewo <ea_pain@yahoo.com>
2023-08-11 08:30:49 +0330

    update to the latest changes

--- src/bot.rs
+++ src/bot.rs
@@ -3,14 +3,15 @@
 
 
 
-use log::{info, error};
+
+use redis::RedisError;
 use tokio::io::AsyncWriteExt;
 use futures_util::StreamExt;
 use futures::future;
 use redis::FromRedisValue;
 use redis::JsonAsyncCommands;
 use redis::cluster::ClusterClient;
-use redis::AsyncCommands; //// this trait is required to be imported in here to call set() methods on the cluster connection
+use redis::AsyncCommands; // this trait is required to be imported in here to call set() methods on the cluster connection
 use redis::RedisResult;
 use serde::{Serialize, Deserialize};
 use std::{rc::Rc, cell::RefCell};
@@ -18,10 +19,17 @@ use std::collections::{HashSet, HashMap};
 use std::{net::SocketAddr, sync::Arc, env};
 use std::time::Duration;
 use dotenv::dotenv;
+use routerify::Router;
+use routerify::Middleware;
+use uuid::Uuid;
+use log::{info, error};
+use once_cell::sync::Lazy;
 use futures::executor::block_on;
 use tokio::sync::oneshot;
-use tokio::sync::Mutex; //// async Mutex will be used inside async methods since the trait Send is not implement for std::sync::Mutex
-use chrono::{TimeZone, Timelike, Datelike, Utc}; //// this trait is rquired to be imported here to call the with_ymd_and_hms() method on a Utc object since every Utc object must be able to call the with_ymd_and_hms() method 
+use tokio::sync::Mutex; // async Mutex will be used inside async methods since the trait Send is not implement for std::sync::Mutex
+use hyper::{Client, Uri, Body};
+use chrono::{TimeZone, Timelike, Datelike, Utc}; // this trait is rquired to be imported here to call the with_ymd_and_hms() method on a Utc object since every Utc object must be able to call the with_ymd_and_hms() method 
+use sysinfo::{NetworkExt, NetworksExt, ProcessExt, System, SystemExt, CpuExt, DiskExt}; // methods of trait DiskExt can be used on each Disk instance to get information of the disk because Disk struct has private methods and we can access them by call the trait DiskExt methods which has been implemented for the Disk struct  
 use serenity::{async_trait, model::prelude::{MessageId, UserId, ChannelId, 
                 interaction::application_command::{CommandDataOption, CommandDataOptionValue}, command::CommandOption}, 
                 framework::standard::{macros::{help, hook}, 
@@ -68,13 +76,65 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>
     dotenv().expect("⚠️ .env file not found");
     let discord_token = env::var("TWISCORD_DISCORD_TOKEN").expect("⚠️ no discord token variable set");
     let serenity_shards = env::var("SERENITY_SHARDS").expect("⚠️ no shards variable set");
-    let io_buffer_size = env::var("IO_BUFFER_SIZE").expect("⚠️ no io buffer size variable set").parse::<u32>().unwrap() as usize; //// usize is the minimum size in os which is 32 bits
-    let (discord_bot_flag_sender, mut discord_bot_flag_receiver) = tokio::sync::mpsc::channel::<bool>(io_buffer_size); //// reading or receiving from the mpsc channel is a mutable process
-    let redis_password = env::var("REDIS_PASSWORD").expect("⚠️ no redis password variable set");
-    let redis_host = std::env::var("REDIS_HOST").expect("⚠️ no redis host variable set");
-    let redis_conn_url = format!("redis://:{}@{}", redis_password, redis_host);
+    let io_buffer_size = env::var("IO_BUFFER_SIZE").expect("⚠️ no io buffer size variable set").parse::<u32>().unwrap() as usize; // usize is the minimum size in os which is 32 bits
+    let (discord_bot_flag_sender, mut discord_bot_flag_receiver) = tokio::sync::mpsc::channel::<bool>(io_buffer_size); // reading or receiving from the mpsc channel is a mutable process
+    
+    /* -=-=-=-=-=-=-=-=-=-=-= REDIS SETUP -=-=-=-=-=-=-=-=-=-=-= */
+
+    let redis_password = env::var("REDIS_PASSWORD").unwrap_or("".to_string());
+    let redis_username = env::var("REDIS_USERNAME").unwrap_or("".to_string());
+    let redis_host = std::env::var("REDIS_HOST").unwrap_or("localhost".to_string());
+    let redis_port = std::env::var("REDIS_PORT").unwrap_or("6379".to_string()).parse::<u64>().unwrap();
+
+    let redis_conn_url = if !redis_password.is_empty(){
+        format!("redis://:{}@{}:{}", redis_password, redis_host, redis_port)
+    } else if !redis_password.is_empty() && !redis_username.is_empty(){
+        format!("redis://{}:{}@{}:{}", redis_username, redis_password, redis_host, redis_port)
+    } else{
+        format!("redis://{}:{}", redis_host, redis_port)
+    };
+
     let redis_client = redis::Client::open(redis_conn_url.as_str()).unwrap();
     let (redis_pubsub_msg_sender, redis_pubsubs_msg_receiver) = tokio::sync::mpsc::channel::<String>(io_buffer_size);
+    
+    /* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+
+
+    /* 
+        for each async and heavy tasks we must use 
+        tokio::spawn(async move{}) channel to avoid 
+        blocking issues, stuck and halting situations 
+    */
+
+
+
+
+
+    // -------------------------------- sending twitter mentions responses to mpsc channel
+    //
+    // -------------------------------------------------------------------------------------------
+    /* 
+        receiving is a mutable process since the underlying 
+        method which is recv() has &mut self as it's first param 
+        which is a mutable pointer to the response_receiver 
+        instance in which mutating the content of the pointer
+        will mutate the content of the instance too.
+    */
+    // let (response_sender, mut response_receiver) = tokio::sync::mpsc::channel::<Vec<schemas::Mention>>(io_buffer_size);
+    // tokio::spawn(async move{
+        
+    //     let res = reqwest::get("https://some-domain/get-twitter-mentions")
+    //         .await
+    //         .unwrap()
+    //         .json()
+    //         .await
+    //         .unwrap();
+
+    //     let Ok(_) = response_sender.send(res).await else{
+    //         panic!("couldn't send to response sender mpsc channel");
+    //     };
+
+    // });
 
 
 
@@ -91,9 +151,32 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>
     */
     tokio::spawn(async move{
 
+        /* comment the following `while let` if we get stuck in here, to avoid blocking issues */
+
+        /* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+        /* RECEIVING FROM THE TWITTER MENTIONS MPSC CHANNEL TO SEND TO THE REDIS PUBSUB CHANNEL */
+        /* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+        /* once we received the mention response from the sender we'll publish it to the redis pubsub channel */
+        // while let Some(res) = response_receiver.recv().await{
+
+        //     let mut redis_conn = redis_client.clone().get_async_connection().await.unwrap();
+        //     info!("📢 publishing twitter user mentions response to redis pubsub [twitter-mentions-response] channel");  
+        //     let pubsub_message = serde_json::to_string_pretty(&res).unwrap();
+        //     let _: Result<_, RedisError> = redis_conn.publish::<String, String, String>("twitter-mentions-response".to_string(), pubsub_message).await;
+            
+        // }
+        /* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+        /* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+        /* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+        
+        /*
+            loop{} event listener to receive from redis pubsub channel inside tokio spawn then 
+            send through the mpsc channel to receive inside another tokio spawn using while let 
+            syntax inside another part of the app which takes care about hanling bot events
+        */
+
         /* we should constantly subscribing to the redis channel once we received the topics from channels */
         loop{
-
 
             let mut redis_conn = redis_client.get_connection().unwrap();
             let mut pubsub = redis_conn.as_pubsub();
@@ -103,6 +186,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>
             let msg = pubsub.get_message().unwrap();
             let payload: String = msg.get_payload().unwrap();
 
+
+            /* UNCOMMENT THE FOLLOWING IF YOU WANT TO SEND TEST MESSAGES */
 
             // /* cloning the redis pubsub mpsc sender to prevent from moving in each iteration before tokio spawn */
             // let redis_pubsub_msg_sender = redis_pubsub_msg_sender.clone();
@@ -125,19 +210,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>
 
                 let mention_text = mention.text;
 
-                /* cloning the redis pubsub mpsc sender to prevent from moving in each iteration before tokio spawn */
+                /* cloning the redis pubsub mpsc sender to prevent from moving in each iteration before going into tokio spawn */
                 let redis_pubsub_msg_sender = redis_pubsub_msg_sender.clone();
 
                 
                 /* 
                                 -------------------------
-                                THIS IS IMPORTANT TO KNOW
+                                    IMPORTANT NOTE
                                 -------------------------
 
                     this tokio spawn will parse and send all the mentions asyncly to 
                     the downside of the redis pubsub mpsc channel, if we don't put 
                     the sending part intside the tokio spawn we'll face a blocking 
-                    situation and will stuck in inside the foor each mention loop.
+                    situation and will stuck in inside the `for each mention` loop.
 
                 */
                 tokio::spawn(async move{
@@ -146,7 +231,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>
                         // ...
 
                         /* sending each mention text that satisfy all the above criteria to the mpsc channel asyncly */
-                        redis_pubsub_msg_sender.clone().send(mention_text.clone()).await;
+                        let Ok(_) = redis_pubsub_msg_sender.clone().send(mention_text.clone()).await else{
+                            panic!("couldn't send to redis pubsub mpsc channel");
+                        };
                     
                 });
                 
